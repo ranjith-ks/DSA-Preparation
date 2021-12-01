6f3cb8f27d14e5dafcab5bbaea928f5f4eb3d612
ranjith-ks <ranjith.ks@outlook.com>
2021-12-01 21:10:43 +0530

    chore: sum of matrix diagonal
    faster java solution

---
+++ LeetCode/MatrixDiagonalSum.java
@@ -0,0 +1,19 @@
+/*
+Given a square matrix mat, return the sum of the matrix diagonals.
+
+Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+*/
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0,i,n=mat.length;
+        for(i=0;i<n;i++)
+        {
+            sum += mat[i][i] + mat[i][n-1-i];
+            if(i==n-1-i)
+                sum -= mat[i][i];
+        }
+
+        return sum;
+    }
+}
