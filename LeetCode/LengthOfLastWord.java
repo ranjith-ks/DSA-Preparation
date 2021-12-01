3247b5841eaf0b48ea510c5eb9a11da0e1efee93
ranjith-ks <ranjith.ks@outlook.com>
2021-12-01 19:13:11 +0530

    feat: last word length
    faster solution

--- LeetCode/LengthOfLastWord.java
+++ LeetCode/LengthOfLastWord.java
@@ -4,10 +4,27 @@ Given a string s consisting of some words separated by some number of spaces, re
 A word is a maximal substring consisting of non-space characters only.
 */
 
+/*
 class Solution {
     public int lengthOfLastWord(String s) {
         String spl[] = s.split(" ");
         int n = spl.length;
         return spl[n-1].length();
+    }
+}
+*/
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0,i;
+        for(i=s.length()-1;i>-1;i--)
+        {
+            if(s.charAt(i)!=' ')
+                count++;
+            else
+                if(count>0)
+                    return count;
+        }
+        return count;
     }
 }
