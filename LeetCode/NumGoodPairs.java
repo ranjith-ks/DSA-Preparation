ed60776bf454fc525c2178818d5d093b5d421083
ranjith-ks <ranjith.ks@outlook.com>
2021-12-06 12:21:39 +0530

    feat:Number of good Pairs
    faster java solution

--- LeetCode/NumGoodPairs.java
+++ LeetCode/NumGoodPairs.java
@@ -4,6 +4,7 @@ Given an array of integers nums, return the number of good pairs.
 A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 */
 
+/*
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         int i,j,n,count = 0;
@@ -15,5 +16,21 @@ class Solution {
                     count++;
         }
         return count;
+    }
+}
+*/
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int i,n,ans=0;
+        int count[] = new int[101];
+        n = nums.length;
+        for(i=0;i<n;i++)
+            count[nums[i]]++;
+        for(i=0;i<101;i++)
+        {
+            ans += (count[i]*(count[i]-1))/2;
+        }
+        return ans;
     }
 }
