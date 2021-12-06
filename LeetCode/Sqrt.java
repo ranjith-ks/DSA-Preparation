be9d94d1d51b41da93092e5e646073eb3dd3bb99
ranjith-ks <ranjith.ks@outlook.com>
2021-12-06 10:44:27 +0530

    chore: Sqrt(n)
    java solution

---
+++ LeetCode/Sqrt.java
@@ -0,0 +1,16 @@
+/*
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+*/
+class Solution {
+    public int mySqrt(int x) {
+        long r = x;
+        while(r*r>x)
+            r = (r + x/r) / 2;
+        return (int)r;
+
+    }
+}
