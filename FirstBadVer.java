85e7eb86b159bfe278e5dfbc77d765a03ebab4a1
ranjith-ks <ranjith.ks@outlook.com>
2021-12-10 21:02:46 +0530

    chore: First Bad Version
    java binary search solution

---
+++ FirstBadVer.java
@@ -0,0 +1,25 @@
+/*
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+*/
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        return findBad(1,n);
+    }
+    public int findBad(int start,int end)
+    {
+        int mid = start + (end-start)/2;
+        if(start<=end)
+        {
+            if(isBadVersion(mid))
+                return findBad(start,mid-1);
+            else
+                return findBad(mid+1,end);
+        }
+        return mid;
+    }
+}
