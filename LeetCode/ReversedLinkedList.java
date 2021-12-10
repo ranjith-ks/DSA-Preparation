a5db47565cd7f4c464bd08b6e5cc20d79809f48c
ranjith-ks <ranjith.ks@outlook.com>
2021-12-10 15:32:40 +0530

    chore: Reversed Linked List
     faster java solution

---
+++ LeetCode/ReversedLinkedList.java
@@ -0,0 +1,15 @@
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+*/
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode nHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return nHead;
+    }
+}
