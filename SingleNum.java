7b4f96d114578d384d69d8b63fc209d5e7056f56
ranjith-ks <ranjith.ks@outlook.com>
2021-12-11 11:27:28 +0530

    chore: Single Number
    faster java solution using xor

---
+++ SingleNum.java
@@ -0,0 +1,14 @@
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+*/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0,n=nums.length,i;
+        for(i=0;i<n;i++)
+            ans ^= nums[i];
+        return ans;
+    }
+}
