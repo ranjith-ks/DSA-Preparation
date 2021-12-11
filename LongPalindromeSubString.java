8b66de7dfbf6ad1bb45f6d1b79bafa0d986899bd
ranjith-ks <ranjith.ks@outlook.com>
2021-12-11 19:31:58 +0530

    chore: Longest Palindromic Substring
    java solution

---
+++ LongPalindromeSubString.java
@@ -0,0 +1,45 @@
+/*
+Given a string s, return the longest palindromic substring in s.
+*/
+class Solution {
+    public String longestPalindrome(String s) {
+        int i,start,end,ansi=0,ansj=0,max=0,n = s.length();
+        for(i=0;i<n;i++)//i is center of string
+        {
+            //Assuming i is odd centered
+            start = i-1;
+            end = i+1;
+            while(start>-1 && end<n && s.charAt(start)==s.charAt(end))
+            {
+                start--;
+                end++;
+            }
+            start++;end--;
+            if(max<end-start+1)
+            {
+                max = end-start+1;
+                ansi = start;
+                ansj = end;
+            }
+            //Assuming i is even centered
+            if(i<n-1 && s.charAt(i)==s.charAt(i+1))
+            {
+                start = i;
+                end = i+1;
+                while(start>-1 && end<n && s.charAt(start)==s.charAt(end))
+                {
+                    start--;
+                    end++;
+                }
+                start++;end--;
+                if(max<end-start+1)
+                {
+                    max = end-start+1;
+                    ansi = start;
+                    ansj = end;
+                }
+            }
+        }
+        return s.substring(ansi,ansj+1);
+    }
+}
