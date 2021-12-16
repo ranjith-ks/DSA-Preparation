e04f7e1b37c7fb0a1d2f93e1a6dfdf09b6255155
ranjith-ks <ranjith.ks@outlook.com>
2021-12-16 11:23:48 +0530

    chore: In Search Of an Easy Problem
    java solution

---
+++ CodeForces/EasyProblem.java
@@ -0,0 +1,32 @@
+/*
+When preparing a tournament, Codeforces coordinators try treir best to make the first problem as easy as possible. This time the coordinator had chosen some problem and asked n people about their opinions. Each person answered whether this problem is easy or hard.
+If at least one of these n people has answered that the problem is hard, the coordinator decides to change the problem. For the given responses, check if the problem is easy enough.
+Input
+The first line contains a single integer n (1≤n≤100) — the number of people who were asked to give their opinions.
+The second line contains n integers, each integer is either 0 or 1. If i-th integer is 0, then i-th person thinks that the problem is easy; if it is 1, then i-th person thinks that the problem is hard.
+Output
+Print one word: "EASY" if the problem is easy according to all responses, or "HARD" if there is at least one person who thinks the problem is hard.
+*/
+
+import java.util.*;
+
+public class EasyProblem
+{
+    public static String difficulty(int[] a,int n)
+    {
+        int i;
+        for(i=0;i<n;i++)
+            if(a[i]==1)
+                return "Hard";
+        return "Easy";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	    int n,i;
+	    n = sc.nextInt();
+	    int respones[] = new int[n];
+	    for(i=0;i<n;i++)
+	        respones[i] = sc.nextInt();
+    	System.out.println(difficulty(respones,n));
+	}
+}
