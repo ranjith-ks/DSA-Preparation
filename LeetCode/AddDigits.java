3281f46538431dbe6ec49ea00d4b1a4c49bec180
ranjith-ks <ranjith.ks@outlook.com>
2021-12-21 15:40:15 +0530

    chore: Add Digits
    faster java solution without loop or recursion

--- LeetCode/AddDigits.java
+++ LeetCode/AddDigits.java
@@ -2,6 +2,7 @@
 Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
 */
 
+/*
 class Solution {
     public int addDigits(int num) {
         int res=0;
@@ -11,5 +12,14 @@ class Solution {
             num /= 10;
         }
         return res<10?res:addDigits(res);
+    }
+}
+*/
+
+class Solution {
+    public int addDigits(int num) {
+        if(num==0)
+            return 0;
+        return num%9==0?9:num%9;
     }
 }
