7ca26f091ccaadde578e325af26afac804aa4342
ranjith-ks <ranjith.ks@outlook.com>
2021-12-27 11:31:07 +0530

    chore: First Unique Character in String
    java solution

---
+++ LeetCode/FirstUnique.java
@@ -0,0 +1,24 @@
+/*
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+*/
+
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> hm = new HashMap<>();
+        int i,n=s.length();
+        for(i=0;i<n;i++)
+        {
+            char c = s.charAt(i);
+            if(hm.containsKey(c))
+                hm.put(c,hm.get(c)+1);
+            else
+                hm.put(c,1);
+        }
+        for(i=0;i<n;i++)
+        {
+            if(hm.get(s.charAt(i))==1)
+                return i;
+        }
+        return -1;
+    }
+}
