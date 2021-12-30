76bdee10629d99e160608e19c7320332fc910e61
ranjith-ks <ranjith.ks@outlook.com>
2021-12-30 11:10:19 +0530

    feat: Find numbers with Even number of digits
    faster java solution

--- LeetCode/NumWithEvendig.java
+++ LeetCode/NumWithEvendig.java
@@ -2,6 +2,7 @@
 Given an array nums of integers, return how many of them contain an even number of digits.
 */
 
+/*
 class Solution {
     public int findNumbers(int[] nums) {
         int i,count=0;
@@ -14,6 +15,19 @@ class Solution {
                 i++;
             }
             if(i%2==0)
+                count++;
+        }
+        return count;
+    }
+}
+*/
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int i,count=0;
+        for(int n:nums)
+        {
+            if( (n>9&&n<100) || (n>999)&&(n<10000) || n==100000)
                 count++;
         }
         return count;
