452310ee39efd87d0f6e7c82d5b912558cf7a949
ranjith-ks <ranjith.ks@outlook.com>
2021-12-31 19:43:16 +0530

    chore: Rotate Array
    faster java solution

---
+++ LeetCode/RotateArray.java
@@ -0,0 +1,24 @@
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+*/
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k%n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+    }
+    public void reverse(int[] a,int i,int j)
+    {
+        int temp;
+        while(i<j)
+        {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i++;j--;
+        }
+    }
+}
