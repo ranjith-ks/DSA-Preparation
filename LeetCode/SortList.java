70ddd6d424b79ef4d011dc195ba51e6a42f37640
ranjith-ks <ranjith.ks@outlook.com>
2022-01-07 20:48:16 +0530

    chore: Sort List
    merge sort java solution

---
+++ LeetCode/SortList.java
@@ -0,0 +1,44 @@
+/*
+Given the head of a linked list, return the list after sorting it in ascending order.
+*/
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode prev=null,slow=head,fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        return merge(l1,l2);
+    }
+    public ListNode merge(ListNode l1,ListNode l2)
+    {
+        ListNode head = new ListNode(0),p = head;
+        while(l1!=null && l2!=null)
+        {
+            if(l1.val<l2.val)
+            {
+                p.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if(l1!=null)
+            p.next = l1;
+        if(l2!=null)
+            p.next = l2;
+        return head.next;
+    }
+}
