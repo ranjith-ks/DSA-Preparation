d06f2587a97ed98871f81d70617a4ff372e9aa2c
ranjith-ks <ranjith.ks@outlook.com>
2022-01-09 20:48:53 +0530

    feat: First Unique Character in String
    faster java solution using alphabet traversal

--- LeetCode/FirstUnique.java
+++ LeetCode/FirstUnique.java
@@ -25,6 +25,7 @@ class Solution {
 }
 */
 
+/*
 class Solution {
     public int firstUniqChar(String s) {
         int i,n=s.length();
@@ -36,5 +37,19 @@ class Solution {
                 return index;
         }
         return -1;
+    }
+}
+*/
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int ans=Integer.MAX_VALUE;
+        for(char c='a';c<='z';c++)
+        {
+            int index = s.indexOf(c);
+            if(index!=-1 && index==s.lastIndexOf(c))
+                ans = Math.min(ans,index);
+        }
+        return ans==Integer.MAX_VALUE?-1:ans;
     }
 }
