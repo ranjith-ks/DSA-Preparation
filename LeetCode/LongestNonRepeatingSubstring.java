fc6f496dc693f56faf9d46fd962fcd70c90f4fce
ranjith-ks <ranjith.ks@outlook.com>
2022-01-18 20:56:11 +0530

    feat: Longest Substring Without Repeating Characters
    O(n) java solution using hashmap

--- LeetCode/LongestNonRepeatingSubstring.java
+++ LeetCode/LongestNonRepeatingSubstring.java
@@ -1,7 +1,7 @@
 /*
 Given a string s, find the length of the longest substring without repeating characters.
 */
-
+/*
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int i,j,n=s.length(),ans=0;
@@ -18,5 +18,22 @@ class Solution {
             ans = Math.max(ans,j-i);
         }
         return ans;
+    }
+}
+*/
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int i=0,pos=0,max = 0,n = s.length();
+        Map<Character,Integer> hm = new HashMap<>();
+        char c;
+        for(i=0;i<n;i++)
+        {
+            c = s.charAt(i);
+            if(hm.containsKey(c))
+                pos = Math.max(pos,hm.get(c)+1);
+            max = Math.max(max,i-pos+1);
+            hm.put(c,i);
+        }
+        return max;
     }
 }
