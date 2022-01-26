9ec1f30f9f62ad8012f230b475aa0ebb84963543
ranjith-ks <ranjith.ks@outlook.com>
2022-01-26 17:11:04 +0530

    feat: All Elements in Two Binary Search Tree
    Inorder traversal and java solution using two stacks

--- LeetCode/AllElements2BST.java
+++ LeetCode/AllElements2BST.java
@@ -2,6 +2,7 @@
 Given two binary search trees root1 and root2, return a list containing all the integers from both trees sorted in ascending order.
 */
 
+/*
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
         List<Integer> res = new ArrayList<>();
@@ -19,5 +20,40 @@ class Solution {
             res.add(r2.val);
         getElements(r1==null?null:r1.left,r2==null?null:r2.left,res);
         getElements(r1==null?null:r1.right,r2==null?null:r2.right,res);
+    }
+}
+*/
+
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        while(root1!=null || root2!=null || !s1.empty() || !s2.empty())
+        {
+            while(root1!=null)
+            {
+                s1.push(root1);
+                root1 = root1.left;
+            }
+            while(root2!=null)
+            {
+                s2.push(root2);
+                root2 = root2.left;
+            }
+            if(s2.empty() || (!s1.empty() && s1.peek().val<=s2.peek().val))
+            {
+                root1 = s1.pop();
+                res.add(root1.val);
+                root1 = root1.right;
+            }
+            else
+            {
+                root2 = s2.pop();
+                res.add(root2.val);
+                root2 = root2.right;
+            }
+        }
+        return res;
     }
 }
