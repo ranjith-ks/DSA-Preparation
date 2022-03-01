da4e504f2949d2f15b14a433466553a59589fbb0
ranjith-ks <ranjith.ks@outlook.com>
2022-03-01 11:04:47 +0530

    chore: Counting Bits
    O(n) solution using Bit Manipulation

---
+++ LeetCode/CountingBits.java
@@ -0,0 +1,12 @@
+/*
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+*/
+
+class Solution {
+    public int[] countBits(int n) {
+        int res[] = new int[n+1];
+        for(int i=1;i<=n;i++)
+            res[i] = res[i>>1] + (i&1);
+        return res;
+    }
+}
