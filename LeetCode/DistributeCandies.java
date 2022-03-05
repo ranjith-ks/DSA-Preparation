d4596f9865756c860fa802d9566710be163af995
ranjith-ks <ranjith.ks@outlook.com>
2022-03-05 09:10:59 +0530

    chore: Distribute Candies
    O(n) solution using Set

---
+++ LeetCode/DistributeCandies.java
@@ -0,0 +1,15 @@
+/*
+Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
+The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
+Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+*/
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<>();
+        int n = candyType.length;
+        for(int i=0;i<n;i++)
+            set.add(candyType[i]);
+        return Math.min(n/2,set.size());
+    }
+}
