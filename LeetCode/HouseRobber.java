0b6545bd7512355111ff9cbbe49c7647815889c3
ranjith-ks <ranjith.ks@outlook.com>
2022-03-13 08:30:35 +0530

    feat: House Robber
    Tabulation Solution

--- LeetCode/HouseRobber.java
+++ LeetCode/HouseRobber.java
@@ -3,6 +3,7 @@ You are a professional robber planning to rob houses along a street. Each house 
 Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 */
 
+/*
 class Solution {
     Map<Integer,Integer> dp = new HashMap<>();
     public int rob(int[] nums) {
@@ -19,5 +20,24 @@ class Solution {
         int y = nums[i] + rob(nums,n,i+3);
         dp.put(i,Math.max(x,y));
         return dp.get(i);
+    }
+}
+*/
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1)
+            return nums[0];
+        int[] dp = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            dp[i] = nums[i];
+            if(i>2)
+                dp[i] += Math.max(dp[i-2],dp[i-3]);
+            else if(i==2)
+                dp[i] += dp[i-2];
+        }
+        return Math.max(dp[n-1],dp[n-2]);
     }
 }
