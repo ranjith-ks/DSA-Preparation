5500928905dfefff442adaf7ee94d4e26a9b6941
ranjith-ks <ranjith.ks@outlook.com>
2022-03-16 09:36:44 +0530

    chore: Jump Game
    O(n) Solution without Extra Space

---
+++ LeetCode/JumpGame.java
@@ -0,0 +1,17 @@
+/*
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+Return true if you can reach the last index, or false otherwise.
+*/
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0,i,n=nums.length;
+        for(i=0;i<n;i++)
+        {
+            if(i>maxReach)
+                return false;
+            maxReach = Math.max(maxReach,i+nums[i]);
+        }
+        return true;
+    }
+}
