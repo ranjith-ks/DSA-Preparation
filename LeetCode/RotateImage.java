a358dab55c75486f332eb248da58ae16d64ae8dc
ranjith-ks <ranjith.ks@outlook.com>
2022-03-18 14:16:55 +0530

    feat: Rotate Image
    O(n) Solution Without Extra Space

--- LeetCode/RotateImage.java
+++ LeetCode/RotateImage.java
@@ -3,6 +3,7 @@ You are given an n x n 2D matrix representing an image, rotate the image by 90 d
 You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 */
 
+/*
 class Solution {
     public void rotate(int[][] matrix) {
         int i,j,n=matrix.length;
@@ -13,5 +14,26 @@ class Solution {
         for(i=0;i<n;i++)
             for(j=0;j<n;j++)
                 matrix[i][j] = res[i][j];
+    }
+}
+*/
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int i,j,n=matrix.length;
+        for(i=0;i<n;i++)
+            for(j=i;j<n;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        for(i=0;i<n;i++)
+            for(j=0;j<n/2;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
     }
 }
