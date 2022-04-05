b65c18532a906706344ea87e3da00b713cca009b
ranjith-ks <ranjith.ks@outlook.com>
2022-04-05 09:43:54 +0530

    chore: Largest Perimeter Triangle
    O(nlogn) Solution

---
+++ LeetCode/LargestPerimeterTriangle.java
@@ -0,0 +1,14 @@
+/*
+Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+*/
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i=n-1;i>1;i--)
+            if(nums[i]<nums[i-1]+nums[i-2])
+                return nums[i]+nums[i-1]+nums[i-2];
+        return 0;
+    }
+}
