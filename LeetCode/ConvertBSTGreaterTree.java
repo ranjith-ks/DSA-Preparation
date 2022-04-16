55d76622441d3e2ec594d3a536e8c0a2797e0fd0
ranjith-ks <ranjith.ks@outlook.com>
2022-04-16 19:48:00 +0530

    chore: Convert BST To Greater Tree
    O(n) Recursive Solution

---
+++ LeetCode/ConvertBSTGreaterTree.java
@@ -0,0 +1,23 @@
+/*
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+*/
+
+class Solution {
+    private int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        convert(root);
+        return root;
+    }
+    private void convert(TreeNode r){
+        if(r==null)
+            return;
+        convert(r.right);
+        r.val += sum;
+        sum = r.val;
+        convert(r.left);
+    }
+}
