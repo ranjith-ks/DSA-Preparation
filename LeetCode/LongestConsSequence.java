c205f8b6959caddef3cfbb2754f6cfdcf7eb840a
ranjith.ks <ranjith.ks@outlook.com>
2022-07-05 07:13:04 +0530

    feat: Longest Consecutive Sequence
    O(N) solution with extra space

--- LeetCode/LongestConsSequence.java
+++ LeetCode/LongestConsSequence.java
@@ -3,6 +3,7 @@ Given an unsorted array of integers nums, return the length of the longest conse
 You must write an algorithm that runs in O(n) time.
 */
 
+/*
 class Solution {
     public int longestConsecutive(int[] nums) {
         Map<Integer,Integer> hm = new HashMap<>();
@@ -14,6 +15,32 @@ class Solution {
             else
                 hm.put(i,1);
             max = Math.max(max,hm.get(i));
+        }
+        return max;
+    }
+}
+*/
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums==null || nums.length==0)
+            return 0;
+        Set<Integer> hs = new HashSet<>();
+        for(int i:nums) hs.add(i);
+        int max = 1;
+        for(int i:nums) {
+            if(hs.remove(i)) {
+                int val = i;
+                int sum = 1;
+                while(hs.remove(val-1))
+                    val--;
+                sum += i - val;
+                val = i;
+                while(hs.remove(val+1))
+                    val++;
+                sum += val - i;
+                max = Math.max(max,sum);
+            }
         }
         return max;
     }
