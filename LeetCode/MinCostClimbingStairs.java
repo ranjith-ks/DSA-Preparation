6fd1e5ba54e242d39a1d9a66b7ed6bec403ca885
ranjith-ks <ranjith.ks@outlook.com>
2022-07-10 07:29:49 +0530

    feat: Min Cost Climbing Stairs
    O(n) Top Down Approach Solution

--- LeetCode/MinCostClimbingStairs.java
+++ LeetCode/MinCostClimbingStairs.java
@@ -4,6 +4,7 @@ You can either start from the step with index 0, or the step with index 1.
 Return the minimum cost to reach the top of the floor.
 */
 
+/*
 class Solution {
     Map<Integer,Integer> dp;
     public int minCostClimbingStairs(int[] cost) {
@@ -18,5 +19,14 @@ class Solution {
             return dp.get(idx);
         dp.put(idx,Math.min(minCost(cost,idx+1),minCost(cost,idx+2)) + cost[idx]);
         return dp.get(idx);
+    }
+}
+*/
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        for(int i=2;i<cost.length;i++)
+            cost[i] = cost[i] + Math.min(cost[i-1],cost[i-2]);
+        return Math.min(cost[cost.length-1],cost[cost.length-2]);
     }
 }
