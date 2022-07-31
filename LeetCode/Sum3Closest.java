8462cd859c2a4d65071e12a094a03384ad8b6557
ranjith.ks <ranjith.ks@outlook.com>
2022-07-31 08:30:56 +0530

    feat: 3Sum Closest
    O(n^2) solution

--- LeetCode/Sum3Closest.java
+++ LeetCode/Sum3Closest.java
@@ -4,6 +4,7 @@ Return the sum of the three integers.
 You may assume that each input would have exactly one solution.
 */
 
+/*
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int i,j,k,n=nums.length,diff,min,sum,ans;
@@ -26,5 +27,29 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+*/
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i < nums.length -2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left  < right) {
+                int sum = nums[i] + nums[right] + nums[left]; 
+                if(Math.abs(target - sum) < Math.abs(target - closest))
+                    closest = sum ;
+                if(sum < target)
+                    left ++;
+                else if(sum > target)
+                    right --;
+                else
+                    return sum;
+            }
+        }
+        return closest;
     }
 }
